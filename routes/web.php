0c0b0130729a1ea496bfde249cc1ef6f6abdeb46
Wildan Malul Azam <malula0101@gmail.com>
2024-05-12 20:49:10 +0700

    moving assets

--- routes/web.php
+++ routes/web.php
@@ -5,14 +5,6 @@ use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
 
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
@@ -32,5 +24,7 @@ Route::get('admin', function(){
 Route::get('user', function(){
     return 'Helo user';
 })->middleware('role:user');
+
+Route::redirect('/', '/login');
 
 require __DIR__.'/auth.php';
